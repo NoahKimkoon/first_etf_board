960405ab4168d192d338c48dafeb5f2afae674ac
noahkimkoon <noah_kim@zionex.com>
2026-04-22 14:39:54 +0900

    Update ETF preset list

--- src/data/etfPreset.js
+++ src/data/etfPreset.js
@@ -3,25 +3,32 @@ export const ETF_PRESET = [
   { id: 1, name: 'KODEX 200', ticker: '069500.KS', category: '국내' },
   { id: 2, name: 'TIGER 200', ticker: '102110.KS', category: '국내' },
   { id: 3, name: 'ACE 미국S&P500', ticker: '360200.KS', category: '국내' },
-  { id: 4, name: 'KODEX 미국S&P500(H)', ticker: '449180.KS', category: '국내' },
-  { id: 5, name: 'KODEX 방산TOP10', ticker: '0080G0.KS', category: '국내' },
-  { id: 16, name: 'TIGER 미국나스닥100', ticker: '133690.KS', category: '국내' },
-  { id: 17, name: 'TIGER 미국 S&P 500', ticker: '360750.KS', category: '국내' },
-  { id: 18, name: 'TIGER 미국 배당다우존스', ticker: '458730.KS', category: '국내' },
+  { id: 4, name: 'ACE 미국나스닥100', ticker: '367380.KS', category: '국내' },
+  { id: 5, name: 'ACE 미국우주테크액티브', ticker: '0180V0.KS', category: '국내' },
+  { id: 6, name: 'KODEX 미국S&P500(H)', ticker: '449180.KS', category: '국내' },
+  { id: 7, name: 'KODEX 방산TOP10', ticker: '0080G0.KS', category: '국내' },
+  { id: 8, name: 'TIGER 미국나스닥100', ticker: '133690.KS', category: '국내' },
+  { id: 9, name: 'TIGER 미국 S&P 500', ticker: '360750.KS', category: '국내' },
+  { id: 10, name: 'TIGER 미국 배당다우존스', ticker: '458730.KS', category: '국내' },
+  
   
   // 미국 ETF
-  { id: 6, name: 'AT&T Inc', ticker: 'T', category: '미국' },
-  { id: 7, name: 'Vanguard S&P 500', ticker: 'VOO', category: '미국' },
-  { id: 8, name: 'Invesco QQQ', ticker: 'QQQ', category: '미국' },
-  { id: 9, name: 'Apple Inc', ticker: 'AAPL', category: '미국' },
-  { id: 10, name: 'Microsoft', ticker: 'MSFT', category: '미국' },
-  { id: 11, name: 'NVIDIA', ticker: 'NVDA', category: '미국' },
-  { id: 12, name: 'Tesla', ticker: 'TSLA', category: '미국' },
+  { id: 101, name: 'AT&T Inc', ticker: 'T', category: '미국' },
+  { id: 102, name: '나이키', ticker: 'NKE', category: '미국' },  
+  { id: 103, name: 'Apple Inc', ticker: 'AAPL', category: '미국' },
+  { id: 104, name: 'Microsoft', ticker: 'MSFT', category: '미국' },
+  { id: 105, name: 'NVIDIA', ticker: 'NVDA', category: '미국' },
+  { id: 106, name: 'Tesla', ticker: 'TSLA', category: '미국' },
+  { id: 107, name: '아마존', ticker: 'AMZN', category: '미국' },
+  { id: 108, name: 'ARM 홀딩스', ticker: 'ARM', category: '미국' },
+  { id: 109, name: '팔란티어', ticker: 'PLTR', category: '미국' },
 
   // 기타
-  { id: 13, name: 'Gold GLD', ticker: 'GLD', category: '원자재' },
-  { id: 14, name: 'Oil USO', ticker: 'USO', category: '원자재' },
-  { id: 15, name: 'TLT 장기채', ticker: 'TLT', category: '채권' },
+  { id: 201, name: '슈왑 미국배당주 ETF', ticker: 'SCHD', category: '미국' },
+  { id: 202, name: 'Invesco QQQ', ticker: 'QQQ', category: '미국' },
+  { id: 203, name: '글로벌엑스 나스닥 100 커버드콜', ticker: 'QYLD', category: '미국' },
+  { id: 204, name: 'Vanguard S&P 500', ticker: 'VOO', category: '미국' },
+  { id: 205, name: 'TLT 장기채', ticker: 'TLT', category: '채권' },
 ];
 
 export default ETF_PRESET;
